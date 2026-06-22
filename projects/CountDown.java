c895b6cf8cfcdc7505a5e26d9958a7b283fdc1f1
aishwarya25-dev <aishwaryaj2510@gmail.com>
2026-06-22 18:06:56 +0530

    Added Count Down

---
+++ projects/CountDown.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CountDown {
+    
+    public static void number(int num)
+    {
+      if(num > 0)
+      {
+        System.out.println(num + " ");
+        number(num - 1);
+      }
+    }
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scn.nextInt();
+
+        number(num);
+
+        scn.close();
+       
+    }
+}
